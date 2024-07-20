1d4a69aa4786de02dd65d363fc00462daaeab689
Waqar Ali <waqarali6489@gmail.com>
2024-07-20 11:07:59 +0500

    Add files via upload

---
+++ defination.py
@@ -0,0 +1,20 @@
+'''WHAT IS PROGRAMMING?
+Just like we use Hindi or English to communicate with each other, we use a 
+programming language like Python to communicate with the computer.
+Programming is a way to instruct the computer to perform various tasks.
+
+WHAT IS PYTHON:
+Python is a simple and easy to understand language which feels like reading simple
+English. This Pseudo code nature is easy to learn and understandable by beginners.
+
+FEATURES OF PYTHON:
+• Easy to understand = Less development time 
+• Free and open source
+• High level language
+• Portable: Works on Linux / Windows / Mac.
+• Fun to work with!
+
+INSTALLATION:
+Python can be easily installed from python.org. When you click on the download 
+button, python can be installed right after you complete the setup by executing the file 
+for your platform.'''
